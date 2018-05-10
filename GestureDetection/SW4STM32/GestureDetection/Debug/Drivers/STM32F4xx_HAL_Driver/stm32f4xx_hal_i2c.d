14b5cb8d8d2d2617093a7ad923eb4996e4273a08
Przemek27 <przemyslaw16@interia.eu>
2018-05-10 18:49:35 +0200

    Initial commit

---
+++ GestureDetection/SW4STM32/GestureDetection/Debug/Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_i2c.d
@@ -0,0 +1,83 @@
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_i2c.o: \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Inc/stm32f4xx_hal_conf.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Inc/main.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Include/core_cm4.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Inc/stm32f4xx_hal_conf.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Inc/main.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Include/core_cm4.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Include/core_cmSimd.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+D:/MCUs/STM32F429/GestureDetection/GestureDetection/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
